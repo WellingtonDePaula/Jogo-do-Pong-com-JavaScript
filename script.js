51f14e7294e540681a946b646677325b549bd896
WellingtonDePaula <wellingtonespiao@gmail.com>
2023-07-10 16:16:00 -0300

    adicionando arquivos

---
+++ script.js
@@ -0,0 +1,11 @@
+var alturaDaTela = window.screen.height
+var larguraDaTela = window.screen.width
+
+function setup() {
+    console.log(alturaDaTela)
+    console.log(larguraDaTela)
+    createCanvas(larguraDaTela, alturaDaTela);
+}
+function draw () {
+    background(0);
+}
