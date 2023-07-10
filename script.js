63a267d475577c073fd8d763fce79e5925073b0b
WellingtonDePaula <wellingtonespiao@gmail.com>
2023-07-10 20:01:56 -0300

    adicionando movimentação e colisão

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-var alturaDaTela = window.innerHeight
-var larguraDaTela = window.innerWidth
+var alturaDaTela = window.innerHeight;
+var larguraDaTela = window.innerWidth;
+var xBolinha = 747.5;
+var yBolinha = 357.5;
+var velocidadeXBolinha = 5;
+var velocidadeYBolinha = 5;
+var diametroBolinha = 30;
+var raioDaBolinha = diametroBolinha/2;
+
+console.log(larguraDaTela)
 
 function setup() {
     createCanvas(larguraDaTela, alturaDaTela);
 }
-function draw () {
+function draw() {
     background(0);
+    desenhaBolinha();
+    movimentaBolinha();
+    colisaoBolinha();
+}
+function desenhaBolinha() {
+    circle(xBolinha, yBolinha, diametroBolinha);
+}
+function movimentaBolinha() {
+    xBolinha += velocidadeXBolinha;
+    yBolinha += velocidadeYBolinha
+}
+function colisaoBolinha() {
+        if (xBolinha + raioDaBolinha > width || xBolinha - raioDaBolinha < 0) {
+        velocidadeXBolinha *= -1;
+    }
+        if (yBolinha + raioDaBolinha > height || yBolinha - raioDaBolinha < 0) {
+    velocidadeYBolinha *= -1;
+    }
 }
